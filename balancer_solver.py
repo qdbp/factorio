8f08ab1c10c4c643edcb7823dd900b6c466c11da
Evgeny Naumov <evgeny.a.naumov@gmail.com>
2019-04-08 12:37:18 -0400

    tentative working version of phase 1 balancer solver

--- balancer_solver.py
+++ balancer_solver.py
@@ -4,11 +4,37 @@ from __future__ import annotations
 
 import typing as ty
 from itertools import product
+from tempfile import NamedTemporaryFile
 
+import numpy as np
 import pulp as pp
 
 
-def solve_balancers(n_inps: int, n_outs: int, max_splitters: int = 50):
+def dicts_to_ndarray(
+        dicts,
+        index_sets: ty.Tuple[ty.Sized, ...],
+        dtype=np.float32,
+) -> np.ndarray:
+
+    shape = tuple(len(ixset) for ixset in index_sets)
+
+    def _rworker(plane: np.ndarray, d: ty.Dict, index_sets):
+        if len(index_sets) > 1:
+            for ex, elem in enumerate(index_sets[0]):
+                _rworker(plane[ex, ...], d[elem], index_sets[1:])
+        else:
+            for ex, elem in enumerate(index_sets[0]):
+                plane[ex] = pp.value(d[elem])
+
+    out = np.zeros(shape, dtype=dtype)
+    _rworker(out, dicts, index_sets)
+
+    return out
+
+
+def solve_balancers(
+        n_inps: int, n_outs: int, max_splitters: int = 50
+) -> np.ndarray:
     '''
     Finds a minimum-splitter balancer using the max-flow formalism with
     extra restrictions.
@@ -24,9 +50,9 @@ def solve_balancers(n_inps: int, n_outs: int, max_splitters: int = 50):
     # but there are only so many hours in a day
     Inps = [f'i{ix}' for ix in range(n_inps)]
     Outs = [f'o{ix}' for ix in range(n_outs)]
-    Spls = [f's{ix}' for ix in range(n_outs)]
+    Spls = [f's{ix}' for ix in range(max_splitters)]
 
-    Nodes = Inps + Outs + Spls
+    Nodes = Inps + Spls + Outs
 
     # flows
     F = pp.LpVariable.dicts("F", (Nodes, Nodes, Inps))
@@ -47,13 +73,19 @@ def solve_balancers(n_inps: int, n_outs: int, max_splitters: int = 50):
 
     # ## RESTRICTIONS
     # # CHAPTER 1: ADJACENCY RESTRICTIONS
-    #  1.1 INPUTS UNIQUELY CONNECTED
+    #  1.1 INPUTS WELL CONNECTED
     for inp in Inps:
+        # each input flows to one node
         prob += pp.lpSum(C[inp][v] for v in Nodes) == 1
+        # nothing flows into an input
+        prob += pp.lpSum(C[u][inp] for u in Nodes) == 0
 
     #  1.2 OUTPUTS UNIQUELY CONNECTED
     for out in Outs:
+        # exactly one node flows into an output
         prob += pp.lpSum(C[u][out] for u in Nodes) == 1
+        # an output flows into nothing
+        prob += pp.lpSum(C[out][v] for v in Nodes) == 0
 
     #  XXX 1.3 and 1.4 disallow dangling splitter inputs and outputs
     for spl in Spls:
@@ -67,11 +99,11 @@ def solve_balancers(n_inps: int, n_outs: int, max_splitters: int = 50):
     for u, v in product(Nodes, Nodes):
         prob += pp.lpSum(F[u][v][i] for i in Inps) <= C[u][v]
 
-    # 2.2 INCOMPRESSIBLE PARTIAL FLOWS
-    for v, i in product(Nodes, Inps):
+    # 2.2 INCOMPRESSIBLE PARTIAL FLOWS FOR INNER NODES
+    for spl, i in product(Spls, Inps):
         prob += (
-            pp.lpSum(F[u][v][i]
-                     for u in Nodes) == pp.lpSum(F[v][w][i] for w in Nodes)
+            pp.lpSum(F[u][spl][i]
+                     for u in Nodes) == pp.lpSum(F[spl][w][i] for w in Nodes)
         )
 
     # 2.3 PROPER FLOW
@@ -92,16 +124,39 @@ def solve_balancers(n_inps: int, n_outs: int, max_splitters: int = 50):
             F[v][out][j] for v in Nodes for j in Inps
         )
 
-    # 3.3 SPLITTERS EQUALIZE
-    # TODO if we don't assume 2 inputs and 2 outputs (the binary c assumption)
-    # we have a term quadratic in C[spl, w]:
-    # sum{w in V}(C[spl, w]) * sum{w in V}(C[spl,w] * F[spl, w, i]) == ...
-    for spl, i, w in product(Spls, Inps, Nodes):
-        prob += 2 * F[spl][w][i] == pp.lpSum(F[u][spl][i] for u in Nodes)
-        # FIXME FIXME
+    # 3.3 SPLITTERS EQUALIZE ALL TYPES
+    for spl in Spls:
+        # n_spl_outs = pp.lpSum(C[spl][w] for w in Nodes)
+        for inp in Inps:
+            inflow = pp.lpSum(F[u][spl][inp] for u in Nodes)
+            for dst in Nodes:
+                # XXX to keep things linear we must hardcode distinct inputs
+                # and distinct outputs UNLESS we have TODO separte indexer sets
+                # for 2->1 and 1->2 wired splitters
+                # afaict this is the only way to encode the splitter condition
+                # (together with flow preservation and capacity respect)
+                prob += 2 * F[spl][dst][i] <= inflow
+
+    with NamedTemporaryFile(suffix='.lp') as f:
+        fn = f.name
+        prob.writeLP(fn)
+        prob.solve()
+
+    if 'Infeasible' in pp.LpStatus[prob.status]:
+        print('INFEASIBLE')
+        return None
+
+    print('flows:')
+    for inp in Inps:
+        print(f'flow for {inp}')
+        print(dicts_to_ndarray(F, (Nodes, Nodes, [inp])).squeeze())
+
+    print('done')
+    return dicts_to_ndarray(C, (Nodes, Nodes))
 
 
 if __name__ == '__main__':
 
     # should give the trivial one-splitter solution, fingers crossed!
-    solve_balancers(2, 2, 5)
+    adj_matrix = solve_balancers(2, 2, 1)
+    print(adj_matrix)
